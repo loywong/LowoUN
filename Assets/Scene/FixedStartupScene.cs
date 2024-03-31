430dfe223169a4bca67b567105ca5a847856fc91
Loy Wong <loywongcom@gmail.com>
2024-03-31 12:33:18 +0800

    Run FixedStartupScene.cs automatically when Editor enter play mode

--- Assets/Scene/FixedStartupScene.cs
+++ Assets/Scene/FixedStartupScene.cs
@@ -1,13 +1,16 @@
 #if UNITY_EDITOR
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // 确保跳转的场景在编辑器的 Build Settings中
-public class FixedStartupScene : MonoBehaviour {
+public class FixedStartupScene : Editor {
     [RuntimeInitializeOnLoadMethod (RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize () {
         // 固定从Entry场景启动
-        SceneManager.LoadScene ("Entry");
+        Scene scene = SceneManager.GetActiveScene ();
+        if (scene.name.Equals ("Splash") || scene.name.Equals ("Login") || scene.name.Equals ("Lobby") || scene.name.Equals ("Battle"))
+            SceneManager.LoadScene ("Entry");
     }
 }
 #endif
