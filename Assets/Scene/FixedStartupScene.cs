208470bde0902f06784e65163e88ac22235a4a15
Loy Wong <loywongcom@gmail.com>
2024-04-07 23:15:49 +0800

    Improve Module-Scene's particular scens judgment

--- Assets/Scene/FixedStartupScene.cs
+++ Assets/Scene/FixedStartupScene.cs
@@ -3,13 +3,27 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// 确保跳转的场景在编辑器的 Build Settings中
+// 确保跳转的场景在编辑器的BuildSettings中
 public class FixedStartupScene : Editor {
+    private static string[] scenePaths = new string[] {
+        "Assets/Scene/Splash.unity",
+        "Assets/Scene/Login.unity",
+        "Assets/Scene/Lobby.unity",
+        "Assets/Scene/Battle.unity"
+    };
+
     [RuntimeInitializeOnLoadMethod (RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize () {
         // 固定从Entry场景启动
         Scene scene = SceneManager.GetActiveScene ();
-        if (scene.name.Equals ("Splash") || scene.name.Equals ("Login") || scene.name.Equals ("Lobby") || scene.name.Equals ("Battle"))
+        bool isParticularScene = false;
+        for (int i = 0; i < scenePaths.Length; i++) {
+            if (string.Equals (scenePaths[i], scene.path)) {
+                isParticularScene = true;
+                break;
+            }
+        }
+        if (isParticularScene)
             SceneManager.LoadScene ("Entry");
     }
 }
