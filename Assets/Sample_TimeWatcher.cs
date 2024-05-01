fa4262c80098cbee0e70cab18151defb8c618ed8
Loy Wong <loywongcom@gmail.com>
2024-05-01 10:36:23 +0800

    Add Util of TimeWatcher

---
+++ Assets/Sample_TimeWatcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using LowoUN.Util;
+using UnityEngine;
+
+public class Sample_TimeWatcher : MonoBehaviour {
+	[Header ("毫秒延迟_不循环")][SerializeField] private uint milliseconds_1 = 5000;
+	[Header ("毫秒延迟_循环")][SerializeField] private uint milliseconds_2 = 3000;
+	[Header ("指定时间点_24小时制")][SerializeField] private string dateTime_specificTime = "13:00:00";
+
+	void Start () {
+		Debug.Log ("Test Start!");
+
+		TimeWatcher.Instance.AddWatcher ("test01", milliseconds_1, false, OnTest01);
+		TimeWatcher.Instance.AddWatcher ("test02", milliseconds_2, true, OnTest02);
+
+		var timeString = DateTime.Now.Date.ToShortDateString () + " " + dateTime_specificTime;
+		Debug.Log ($"timeString {timeString}");
+		TimeWatcher.Instance.AddWatcher ("test03", DateTime.Parse (timeString), false, OnTest03);
+		TimeWatcher.Instance.AddWatcher ("test04", DateTime.Parse (timeString), true, OnTest04);
+	}
+
+	private void OnTest01 () {
+		Debug.Log ("[test01] run 5's later once");
+	}
+
+	private void OnTest02 () {
+		Debug.Log ("[test02] run every 3's");
+	}
+
+	private void OnTest03 () {
+		Debug.Log ($"[test03] run at {dateTime_specificTime}");
+	}
+
+	private void OnTest04 () {
+		Debug.Log ($"[test04] run at {dateTime_specificTime} everyday!");
+	}
+
+	void OnGUI () {
+		GUI.skin.button.fontSize = 20;
+		GUI.skin.button.alignment = TextAnchor.MiddleLeft;
+
+		if (GUI.Button (new Rect (30, 5, 240, 40), "RemoveWatcher Test01"))
+			TimeWatcher.Instance.RemoveWatcher ("test01");
+
+		if (GUI.Button (new Rect (30, 55, 240, 40), "RemoveWatcher Test02"))
+			TimeWatcher.Instance.RemoveWatcher ("test02");
+
+		if (GUI.Button (new Rect (30, 105, 240, 40), "RemoveWatcher Test03"))
+			TimeWatcher.Instance.RemoveWatcher ("test03");
+
+		if (GUI.Button (new Rect (30, 155, 240, 40), "RemoveWatcher Test04"))
+			TimeWatcher.Instance.RemoveWatcher ("test04");
+	}
+}
---
+++ Assets/Util/TimeWatcher.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+namespace LowoUN.Util {
+    public class TimeWatcher : ManagerMono<TimeWatcher> {
+        private Dictionary<string, WatchData> name2watch = new Dictionary<string, WatchData> ();
+        private Dictionary<string, Action> name2action = new Dictionary<string, Action> ();
+        private Queue<Action> cbqueue = new Queue<Action> ();
+
+        /// <summary>
+        /// add delay event
+        /// </summary>
+        /// <param name="name">uniqueue name for delay event</param>
+        /// <param name="mseconds">milliseconds for delay</param>
+        /// <param name="loop">repeat or not</param>
+        public void AddWatcher (string name, uint mseconds, bool loop, Action callback, bool isTimeScaleAff = true) {
+            if (mseconds <= 0) {
+                Debug.LogWarning ("watch time can not be less or equal 0");
+                return;
+            }
+
+            if (name2watch.ContainsKey (name)) {
+                Debug.LogWarning ($"This name {name} of timer is exist!");
+                return;
+            }
+
+            uint ms = isTimeScaleAff ? (uint) ((float) mseconds * (1f / Time.timeScale)) : mseconds;
+            if (ms <= 0)
+                return;
+
+            name2watch[name] = new WatchData (name, ms, loop);
+            name2action[name] = callback;
+            name2watch[name].Start ();
+        }
+
+        /// <summary>
+        /// add specific time event
+        /// </summary>
+        /// <param name="name">uniqueue name for each delay event</param>
+        /// <param name="time">specific time</param>
+        /// <param name="loop">repeat ot not, if true, every 24 houra work!</param>
+        public void AddWatcher (string name, DateTime time, bool loop, Action callback) {
+            if (name2watch.ContainsKey (name)) {
+                Debug.LogWarning ($"This name {name} of timer is exist!");
+                return;
+            }
+
+            var w = new WatchData (name, time, loop);
+            name2watch[name] = w;
+            name2action[name] = callback;
+
+            w.Start ();
+        }
+
+        // /// <summary>
+        // /// add specific time event with time string
+        // /// </summary>
+        // /// <param name="name">uniqueue name for each delay event</param>
+        // /// <param name="time">specific time string include hour, minute, second, eg."18:00:00"</param>
+        // /// <param name="loop">repeat or not</param>
+        // /// <param name="callback"></param>
+        // public void AddWatcher (string name, string time, bool loop, Action callback) {
+        //     AddWatcher (name, DateTime.Parse (time), loop, callback);
+        // }
+
+        public void RemoveWatcher (string name) {
+            if (!name2watch.ContainsKey (name)) {
+                Debug.Log($"RemoveWatcher has no event key:{name}");
+                return;
+            }
+
+            name2watch[name].End ();
+            name2watch.Remove (name);
+            name2action.Remove (name);
+            Debug.Log($"RemoveWatcher event key: {name}, succ!");
+        }
+
+        public void OnCallback (string name) {
+            if (!name2action.ContainsKey (name))
+                return;
+
+            cbqueue.Enqueue (name2action[name]);
+            if (!name2watch[name].loop) {
+                RemoveWatcher (name);
+            }
+        }
+
+        void Update () {
+            if (cbqueue.Count > 0) {
+                Action action = cbqueue.Dequeue ();
+                action?.Invoke ();
+            }
+        }
+
+        void OnApplicationQuit () {
+            foreach (var kvp in name2watch) {
+                kvp.Value.End ();
+            }
+
+            name2watch.Clear ();
+            name2action.Clear ();
+            cbqueue.Clear ();
+        }
+
+        // Check time event is exist
+        public bool ContainKey (string key) {
+            if (key == null) {
+                Debug.LogWarning("Invalid key:null");
+                return false;
+            }
+
+            return name2watch.ContainsKey (key);
+        }
+    }
+
+    public class WatchData {
+        private string name;
+        private Timer timer;
+        // private Action<string> callback;
+        public bool loop { private set; get; }
+        private bool isForDateTime;
+
+        public WatchData (string name, uint mseconds, bool loop) {
+            isForDateTime = false;
+
+            this.name = name;
+            this.loop = loop;
+
+            timer = new Timer (mseconds);
+            timer.Elapsed += OnElapsed;
+            timer.AutoReset = loop;
+        }
+
+        public WatchData (string name, DateTime time, bool loop) {
+            isForDateTime = true;
+
+            this.name = name;
+            this.loop = loop;
+
+            TimeSpan span = time - DateTime.Now;
+            if (span.TotalMilliseconds > 0) {
+                timer = new Timer (span.TotalMilliseconds);
+            } else {
+                span = time.AddDays (1) - DateTime.Now;
+                timer = new Timer (span.TotalMilliseconds);
+            }
+            timer.Elapsed += OnElapsed;
+            timer.AutoReset = false;
+        }
+
+        private void OnElapsed (object sender, ElapsedEventArgs e) {
+            if (!isForDateTime) {
+                if (!loop)
+                    timer.Stop ();
+
+                TimeWatcher.Instance.OnCallback (name);
+            } else {
+                timer.Stop ();
+                TimeWatcher.Instance.OnCallback (name);
+
+                if (loop) {
+                    timer.Interval = new TimeSpan (24, 0, 0).Milliseconds;
+                    timer.Start ();
+                }
+            }
+        }
+
+        public void Start () {
+            timer.Start ();
+        }
+
+        public void Stop () {
+            timer.Stop ();
+        }
+
+        public void End () {
+            timer.Stop ();
+            timer.Elapsed -= OnElapsed;
+            timer.Dispose ();
+        }
+    }
+}
